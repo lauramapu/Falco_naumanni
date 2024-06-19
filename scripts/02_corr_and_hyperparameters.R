d8e3fdf8282cfedc77c742e35d9d67d59e901a8f
Laura Mapu <lauramapu95@gmail.com>
2024-06-19 15:34:28 +0200

    ranger implemented, added R-squared and conditional case weights

--- scripts/02_corr_and_hyperparameters.R
+++ scripts/02_corr_and_hyperparameters.R
@@ -1,6 +1,6 @@
 # hyperparameter tuning
 
-library(randomForest)
+library(ranger)
 library(caret)
 library(dplyr)
 library(tictoc)
@@ -41,11 +41,12 @@ write.csv2(grid, "data/grid_hyperpar.csv", row.names=F)
 # run models iterating through each hyperpar combination
 tic("random hyperparameters")
 metrics <- foreach(i = 1:nrow(grid),
-                   .packages=c("randomForest", "sf", "dplyr", "caret"),
+                   .packages=c("ranger", "sf", "dplyr", "caret"),
                    .combine='rbind') %:% # iterations through hyperpar combinations
   
   foreach(j = 1:5,
-          .combine='rbind') %dopar% { # iterations through random cv folds (5)
+          .combine='rbind',
+          verbose=T) %dopar% { # iterations through random cv folds (5)
             
             # set unique seed for each fold
             set.seed(j**2)
@@ -55,49 +56,72 @@ metrics <- foreach(i = 1:nrow(grid),
             train <- dataset[index, ]
             test <- dataset[-index, ]  
             
-            # extracting sample size
-            prNum <- nrow(dataset) - as.numeric(table(train$Total)['0']) # number of presences
+            # calculate case weights in training
+            # class frecs
+            freq_zero <- sum(train$Total == 0)
+            freq_nonzero <- sum(train$Total != 0)
+            # calculate if frec zero > non_zero
+            if (freq_zero > freq_nonzero) {
+              # inversaly proportional weights to the frecs
+              weight_zero <- 1 / freq_zero
+              weight_nonzero <- 1 / freq_nonzero
+              
+              # Asignar los pesos a cada observación
+              train <- train %>%
+                mutate(case_weight = ifelse(response == 0, weight_zero, weight_nonzero))
+              
+              # normalize weights to sum up 1
+              train$case_weight <- train$case_weight / sum(train$case_weight)
+            } else {
+              # assign same weight to all obs
+              train <- train %>%
+                mutate(case_weight = 1 / n())
+            }
             
             # train the model
-            model <- randomForest(Total ~ . -X -Y -geometry -Ano_CS,
-                                  data = train,
-                                  sampsize = prNum,
-                                  mtry = grid[i,1],
-                                  ntree = 1000,
-                                  nodesize = grid[i,3])
+            model <- ranger(Total ~ . -X -Y -geometry -Ano_CS -case_weight,
+                            data = train,
+                            case.weights = train$case_weight,
+                            mtry = grid[i,1],
+                            num.tree = 1000,
+                            min.node.size = grid[i,2],
+                            importance='none')
             
             # generate training predictions (only select probs of presence)
-            preds <- predict(model)
-            train$pred <- preds
+            preds <- predict(model, data=train)
+            train$pred <- preds[[1]]
             
             # calculate metrics (caret)
             mae_train <- MAE(preds, train$Total)
             mse_train <- mean((preds - train$Total)^2)
             rmse_train <- RMSE(preds, train$Total)
+            Rsq_train <- 1 - (sum((train$Total - preds)^2) / sum((train$Total - mean(train$Total))^2))
             
             # generate testing predictions and binaries from maxSSS
-            preds <- predict(model, newdata = test)
-            test$pred <- preds
+            preds <- predict(model, data=test)
+            test$pred <- preds[[1]]
             
             # calculate metrics (caret)
             mae_test <- MAE(preds, test$Total)
             mse_test <- mean((preds - test$Total)^2)
             rmse_test <- RMSE(preds, test$Total)
+            Rsq_test <- 1 - (sum((test$Total - preds)^2) / sum((test$Total - mean(test$Total))^2))
             
             # store all the relevant values in the metrics dataframe
             # column_names <- c('mtry', 'ntree', 'nodesize','thr_value',
             # 'Sens_train', 'F1_train','B.Accuracy_train','TSS_train','AUC_train',
             # 'Sens_test', 'F1_test', 'B.Accuracy_test','TSS_test','AUC_test')
             
-            metrics <- c(i, j, grid[i,1], grid[i,2], mae_train, mse_train, rmse_train, mae_test, mse_test, rmse_test)
+            metrics <- c(i, j, grid[i,1], grid[i,2], mae_train, mse_train, rmse_train, Rsq_train,
+                         mae_test, mse_test, rmse_test, Rsq_test)
             return(metrics)
           }
 toc() 
 
 # assign col names
 metrics <- as.data.frame(metrics)
-column_names <- c('combination', 'fold', 'mtry', 'nodesize', 'MAE_train', 'MSE_train', 'RMSE_train',
-                  'MAE_test', 'MSE_test', 'RMSE_test')
+column_names <- c('combination', 'fold', 'mtry', 'nodesize', 'MAE_train', 'MSE_train', 'RMSE_train', 'Rsquared_train',
+                  'MAE_test', 'MSE_test', 'RMSE_test', 'Rsquared_test')
 colnames(metrics) <- column_names
 write.csv2(metrics, "results/hyperparameters.csv", row.names = F)
 # metrics <- read.csv2("results/hyperparameters.csv")
